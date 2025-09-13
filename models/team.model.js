1a96ddd81f70cb9b6921547aab637d7a20a220e7
Dawaman43 <Dawitworkujima@gmail.com>
2025-09-13 11:39:54 +0300

    update in user model

--- models/team.model.js
+++ models/team.model.js
@@ -1,12 +1,10 @@
 const db = require("../configs/db.config");
 
 const Team = {
-  // Create a new team
   create: (teamData, callback) => {
     db.query("INSERT INTO teams SET ?", teamData, callback);
   },
 
-  // Fetch all teams (basic, for admin use)
   findAll: (callback) => {
     const query = `
       SELECT t.id, t.name, t.description, t.created_at,
@@ -20,17 +18,14 @@ const Team = {
     db.query(query, callback);
   },
 
-  // Fetch team by ID
   findById: (id, callback) => {
     db.query("SELECT * FROM teams WHERE id = ?", [id], callback);
   },
 
-  // Fetch team by name
   findByName: (name, callback) => {
     db.query("SELECT * FROM teams WHERE name = ?", [name], callback);
   },
 
-  // Assign leader to a team
   assignLeader: (teamId, leaderId, callback) => {
     db.query(
       "UPDATE teams SET leader_id = ? WHERE id = ?",
@@ -39,17 +34,14 @@ const Team = {
     );
   },
 
-  // Update a team
   update: (teamId, teamData, callback) => {
     db.query("UPDATE teams SET ? WHERE id = ?", [teamData, teamId], callback);
   },
 
-  // Delete a team
   delete: (teamId, callback) => {
     db.query("DELETE FROM teams WHERE id = ?", [teamId], callback);
   },
 
-  // Fetch members of a team
   findMembersByTeamId: (teamId, callback) => {
     db.query(
       "SELECT id, name, email, jobTitle, role, team_id FROM users WHERE team_id = ?",
